749d441b73153e29c648618e8f37329cd8a4b74c
Mark Miranda <weatherby@m2-MBA>
2023-11-08 22:10:24 -0700

    Add user model & migration

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    sequence(:email) { |n| "test#{n}@example.com" }
+    password_digest { "password" }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    nickname { Faker::Superhero.name }
+  end
+end
