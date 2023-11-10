eb42e41e98bf21c1b67595ca5b87b3aaf5a578f4
Mark Miranda <weatherby@m2-MBA>
2023-11-09 18:05:05 -0700

    Add views for user to sign up or in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,29 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(user_params)
-    if @user.save
-      session[:user_id] = @user.id
-      flash[:notice] = "Hey! It worked!"
-      redirect_to dashboard_path
+    @user = User.find_by(email: user_params[:email])
+    if @user&.authenticate(user_params[:password])
+      sign_user_in
+    elsif @user
+      flash[:alert] = "Something went wrong"
+      redirect_to sign_up_or_in_path
+    else
+      create_user
+      sign_user_in
     end
   end
 
   private
+
+  def create_user
+    @user = User.create(user_params)
+  end
+
+  def sign_user_in
+    session[:user_id] = @user.id
+    flash[:notice] = "Hey! It worked!"
+    redirect_to dashboard_path
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :first_name, :last_name, :nickname)
