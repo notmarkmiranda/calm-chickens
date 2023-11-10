a881c63ddb0369ca8c230d9c4b438001379c5cd4
Mark Miranda <weatherby@m2-MBA>
2023-11-10 13:13:25 -0700

    add validation around pool#start_datetime

--- app/models/pool.rb
+++ app/models/pool.rb
@@ -1,4 +1,14 @@
 class Pool < ApplicationRecord
   belongs_to :superadmin, class_name: "User", foreign_key: "user_id"
   validates :name, presence: true, uniqueness: { case_sensitive: false }
+
+  validate :start_datetime_over_one_day_away
+
+  private
+
+  def start_datetime_over_one_day_away
+    if start_datetime.present? && (start_datetime < 24.hours.from_now)
+      errors.add(:start_datetime, "must be greater than 24 hours in the future")
+    end
+  end
 end
--- spec/models/pool_spec.rb
+++ spec/models/pool_spec.rb
@@ -6,5 +6,20 @@ RSpec.describe Pool, type: :model do
     it { should belong_to :superadmin }
     it { should validate_presence_of :name }
     it { should validate_uniqueness_of(:name).case_insensitive }
+
+    context "start_datetime is in the future" do
+      let(:pool) { build(:pool) }
+      it "does not have errors when the date is in the future" do
+        pool.start_datetime = 25.hours.from_now
+        expect(pool.valid?).to eq(true)
+        expect(pool.errors).to be_empty
+      end
+
+      it "has errors when the date is in the future" do
+        pool.start_datetime = 23.hours.from_now
+        expect(pool.valid?).to eq(false)
+        expect(pool.errors.full_messages).to eq(["Start datetime must be greater than 24 hours in the future"])
+      end
+    end
   end
 end
