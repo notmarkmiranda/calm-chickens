0e51b782e4c8b4bfb47e45772ee985a60c5bae7a
Mark Miranda <weatherby@m2-MBA>
2023-11-17 21:34:14 -0700

    Add memberships#index view

--- app/controllers/memberships_controller.rb
+++ app/controllers/memberships_controller.rb
@@ -1,5 +1,8 @@
 class MembershipsController < ApplicationController
+  before_action :require_user
+
   def index
-    @memberships = current_user.memberships
+    @memberships = current_user.memberships.includes(:pool)
+    @superadmin_pools = current_user.pools
   end
 end
--- app/models/membership.rb
+++ app/models/membership.rb
@@ -2,6 +2,7 @@ class Membership < ApplicationRecord
   belongs_to :user
   accepts_nested_attributes_for :user
   belongs_to :pool
+  delegate :name, to: :pool, prefix: true
 
   enum role: { 'member': 0, 'admin': 1 }
   enum status: { 'pending': 0, 'active': 1, 'inactive': 2 }
